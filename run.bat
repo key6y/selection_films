90aae73c89834dd9eeb71c31da5ca27b99514769
Konstantin <lunin20040108@yandex.ru>
2025-05-03 11:20:47 +0600

    создание exe

--- run.bat
+++ run.bat
@@ -1,8 +1,42 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFKuL/N6+GGStCLkT6ezo0+GOrEINW+ssc7DX1bOBH+kQ5UvbJ8J6jyoN1sIPA3s=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJOleRaGByKq1/NQz2ysK1wXI1OPy4eWXrUQOX6w6YIq7
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
-cd "D:\курсач бд"
+cd /d "%~dp0"
 echo Stopping existing containers...
 docker-compose down
 echo Starting database in background...
 docker-compose up -d db
 echo Starting application in interactive mode...
 docker-compose run --rm app
+pause
